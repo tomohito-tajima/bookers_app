ec2f9a1399330c8446a3c97fe9ff5eed852713d6
tomohito tajima <tomo322017@gmail.com>
2021-10-13 07:41:33 +0000

    Finish テスト

--- db/schema.rb
+++ db/schema.rb
@@ -12,13 +12,6 @@
 
 ActiveRecord::Schema.define(version: 2021_10_10_052908) do
 
-  create_table "bookers", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "books", force: :cascade do |t|
     t.string "title"
     t.text "body"
